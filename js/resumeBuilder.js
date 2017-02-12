f3085f2a463881c85fbbe0fbfdfc0f2e1970e8da
Shivani Narang <you@example.com>
2017-02-12 19:24:31 +0530

    Test

---
+++ js/resumeBuilder.js
@@ -0,0 +1,205 @@
+var bio = {
+    "name": "Shivani Narang",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "886-121-4572",
+        "email": "shivanisnarang@gmail.com",
+        "github": "snarang",
+        "twitter": "@snarang",
+        "location": "Bangalore",
+    },
+    "welcomeMessage": " Welcome to my Resume!",
+    "skills": ["Hard Working", "Postive Attitude", "Team Player", "Optimist"],
+    "bioPic": "images/a3.jpg",
+};
+
+bio.display = function() {
+
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedBioPic = HTMLbioPic.replace('%data%', bio.bioPic);
+    var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+    var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+    var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
+    var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+    var formattedwelcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedName);
+    $('#header').append(formattedBioPic);
+    $('#topContacts').append(formattedMobile);
+    $('#topContacts').append(formattedEmail);
+    $('#topContacts').append(formattedGithub);
+    $('#topContacts').append(formattedTwitter);
+    $('#topContacts').append(formattedLocation);
+    $('#footerContacts').append(formattedMobile);
+    $('#footerContacts').append(formattedEmail);
+    $('#footerContacts').append(formattedGithub);
+    $('#footerContacts').append(formattedTwitter);
+    $('#footerContacts').append(formattedLocation);
+
+    $('#header').append(formattedwelcomeMsg);
+    bio.skills.forEach(function(skill) {
+    console.log(skill);
+    var formattedSkill = HTMLskills.replace("%data%", skill);
+    $("#skills").append(formattedSkill);
+});
+};
+
+bio.display();
+
+if(bio.skills.length > 0) {
+      $("#header").append(HTMLskillsStart);
+      var formattedSkill = HTMLskills.replace("%data%" , bio.skills[0]);
+      $("#skills").append(formattedSkill);
+      formattedSkill = HTMLskills.replace("%data%" , bio.skills[1]);
+      $("#skills").append(formattedSkill);
+      formattedSkill = HTMLskills.replace("%data%" , bio.skills[2]);
+      $("#skills").append(formattedSkill);
+      formattedSkill = HTMLskills.replace("%data%" , bio.skills[3]);
+      $("#skills").append(formattedSkill);
+ }
+
+var education = { 
+  "schools" : [
+       {
+        "name" : "NIT",
+        "location" : "Punjab, India",
+        "degree" : "Masters of Technology",
+        "majors" : ["EC"],
+        "dates"  : 2008,
+        "url" : "www.google.com"
+       },
+       {
+        "name" : "Guru Nanak Dev University",
+        "location" : "Punjab, India",
+        "degree" : "Bachelor of Technology",
+        "majors" : ["EC"],
+        "dates"  : 2006,
+        "url" : "www.google.com"
+       }
+       ],
+    "onlineCourses" : [
+     {
+       "title" : "Front_End_Development_Course",
+       "school" : "Udacity",
+       "dates" : 2016,
+       "url" : "https://in.udacity.com/"
+     }
+   ]
+ };
+ 
+function displayEducation() {
+  for (var i = 0; i < education.schools.length; i ++) {
+    $("#education").append(HTMLschoolStart);
+
+    var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name);
+    var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+    var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+    var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
+    var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+    $(".education-entry:last").append(formattedName);
+    $(".education-entry:last").append(formattedDates);
+    $(".education-entry:last").append(formattedLocation);
+    $(".education-entry:last").append(formattedMajor);
+    $(".education-entry:last").append(formattedDegree);
+}
+    $("#education").append(HTMLonlineClasses);
+    $("#education").append(HTMLschoolStart);
+
+    for (i = 0; i < education.onlineCourses.length; i++)
+        {
+            var formattedOnlineCourse= HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
+            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+            var formattedOnlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
+            var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
+
+            $(".education-entry:last").append(formattedOnlineCourse + formattedOnlineSchool);
+            $(".education-entry:last").append(formattedOnlineDate);
+            $(".education-entry:last").append(formattedOnlineURL);
+        }
+  
+}
+displayEducation();
+
+var work = {
+  "jobs" : [
+     {
+         "employer" : "Rentsher",
+         "title" : "Web Developer",
+         "location" : "Bangalore",
+         "dates" : 2016,
+         "description" : "RentSher is online product rentals marketplace. It provides all home, office, event rental requirements delivered at your doorstep with assured quality and guarantee on the products. The products are provided by other people and vendors, but selected and curated by our dedicated team. RentSher is a leader in online costumes for rent especially for children, or for specific requirements like dance, superheroes, professional costumes and more."
+   },
+   {
+         "employer" : "Capgemini",
+         "title" : "Consultant",
+         "location" : "Mumbai",
+         "dates" : 2008,
+         "description" : "Capgemini Consulting teams design winning strategies by harnessing the power of the new digital economy and deliver value and performance through the mastery of digital advances, information insight and business transformation."
+   }
+  ]
+ };
+
+ function displayWork() {
+ for (var i = 0; i < work.jobs.length; i++){
+     $("#workExperience").append(HTMLworkStart);
+     var formattedEmployer = HTMLworkEmployer.replace("%data%" , work.jobs[i].employer);
+     var formattedTitle = HTMLworkTitle.replace("%data%" , work.jobs[i].title);
+     var formattedEmployerTitle = formattedEmployer + formattedTitle;
+     $(".work-entry:last").append(formattedEmployerTitle);
+
+     var formattedDates = HTMLworkDates.replace("%data%" , work.jobs[i].dates);
+     $(".work-entry:last").append(formattedDates);
+
+     var formattedDescription = HTMLworkDescription.replace("%data%" , work.jobs[i].description);
+     $(".work-entry:last").append(formattedDescription);
+ }
+}
+
+displayWork();
+
+ var projects = {
+  "projects" : [ 
+     {
+          "title" : "Building an ecommerce platform to perform peer to peer online rental for Rentsher",
+          "dates" : 2016,
+          "description" : "Built online platform using Bootstrap and angularjs",
+          "images" : ["images/delhi.jpg","images/yoga.png"]
+    },
+    {
+      "title" : "CareerMyles",
+          "dates" : 2016,
+          "description" : "Built HR Recruitment webpage using Bootstrap framework",
+          "images" : ["images/careermyles.png","images/docsapp.png"]
+    }
+  ]
+};
+
+function projectWork() {
+    for (var i =0; i < projects.projects.length; i++) {
+         $("#projects").append(HTMLprojectStart);
+
+         var formattedTitle = HTMLprojectTitle.replace("%data%" , projects.projects[i].title);
+         $(".project-entry:last").append(formattedTitle);
+
+         var formattedDates = HTMLprojectDates.replace("%data%" , projects.projects[i].dates);
+         $(".project-entry:last").append(formattedDates);
+
+         var formattedDescription = HTMLprojectDescription.replace("%data%" , projects.projects[i].description);
+         $(".project-entry:last").append(formattedDescription);
+
+         if (projects.projects[i].images.length > 0){
+                for (var image = 0; image < projects.projects[i].images.length; image++) {
+                   var formattedImage = HTMLprojectImage.replace("%data%" , projects.projects[i].images[image]);
+                   $(".project-entry:last").append(formattedImage);
+                }
+         }
+    }
+}
+
+projectWork();
+
+$("#mapDiv").append(googleMap);
+
